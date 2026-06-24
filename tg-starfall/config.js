e89888e14165b7d3f9c7c0acfd372c7f07a7af04
Mr Tim <timofeymarkin98@gmail.com>
2026-06-24 21:19:53 +0100

    Add Starfall ad config hook

---
+++ tg-starfall/config.js
@@ -0,0 +1,4 @@
+// Public, non-secret Mini App config.
+// Set this to the AdsGram rewarded/interstitial block id when the publisher
+// placement is approved, then redeploy.
+window.STARFALL_ADSGRAM_BLOCK_ID = "";
